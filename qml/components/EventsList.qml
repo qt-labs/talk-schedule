qt-labs/talk-schedule

commit 8f2c30428011c3048eb7abc6295f774b46c53494

Disable dynamicSortFilter for the proxy model
There are some models issues when adding/removing favorite events from
the Presentations page.

Adding/removing favorites updates the eventModel. The eventModel
is sometimes used as source model for the SortProxyModel, as in the
EventsList. As stated by the documentation, dynamicSortFilter should be
set to false in that case so that the proxy model is not automatically
sorted every time the model change. In our case, only the favorite field
is updated so there is no need to call sort() again.

Change-Id: I01a752748a88f4033349b9cb7af5dba47e8f34ae
Reviewed-by: Niels Weber <niels.weber@digia.com>

src/sortfiltermodel.cpp
@@ -46,6 +46,7 @@ SortFilterModel::SortFilterModel(QObject *parent)
 {
     connect(this, SIGNAL(sourceModelChanged()), SIGNAL(modelChanged()));
     connect(this, SIGNAL(sortRoleChanged()), SLOT(manualSort()));
+    setDynamicSortFilter(false);
 }
 
 void SortFilterModel::setModel(QObject *model)

commit 451699d9d4ec1ebf9ba7288611d1a5011892ed84

Fix Favorites view not being updated after a favorite is removed
Problem was introduced with 8f2c30428011c304. The filter needs to be
invalidated.

Change-Id: I3f595cc38a66445f7561a66c6aad4d752c353923
Reviewed-by: Niels Weber <niels.weber@digia.com>

qml/components/EventsList.qml
@@ -60,11 +60,16 @@ Item {
         width: parent.width
         clip: true
         model: SortFilterModel {
-                           sortRole: "start"
-                           filterRole: "favorite"
-                           filterRegExp: eventsList.isFavoriteView ? new RegExp("true") : new RegExp()
-                           model: ModelsSingleton.eventModel
-                       }
+            id: sortModel
+            sortRole: "start"
+            filterRole: "favorite"
+            filterRegExp: eventsList.isFavoriteView ? new RegExp("true") : new RegExp()
+            model: ModelsSingleton.eventModel
+        }
+        Connections {
+            target: ModelsSingleton.eventModel
+            onDataChanged: if (eventsList.isFavoriteView) sortModel.filter()
+        }
         delegate: EventsListDelegate {}
     }
 }

src/sortfiltermodel.cpp
@@ -109,3 +109,9 @@ void SortFilterModel::manualSort()
 {
     sort(0, sortOrder());
 }
+
+void SortFilterModel::filter()
+{
+    QSortFilterProxyModel::invalidate();
+    manualSort();
+}

src/sortfiltermodel.h
@@ -65,6 +65,7 @@ public:
     Q_INVOKABLE void set(int row, const QVariant &data, const QString &role);
     Q_INVOKABLE QVariant indexOf(const QString &role, QVariant value);
     Q_INVOKABLE int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    Q_INVOKABLE void filter();
 
 public Q_SLOTS:
     void manualSort();